e5feae282a88cfa9c6a58fc61cfe2a85becbd9a7
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-04-01 07:26:27 +0000

    maled_model

---
+++ db/migrate/20250401070832_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.integer :user_id, null: false
+      t.integer :group_id
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :category, null: false
+      t.timestamps
+    end
+  end
+end
