41efb9ad44e9760479b825f9d5277ad98dd3888a
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-04-08 08:19:32 +0000

    [Finish]item.new

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :user
-  delongs_to :group
-  enum genre: { cosmetics: 0, daily_necessities: 1, groceries: 2, supplement: 3}
+  belongs_to :group
+  enum category: { cosmetics: 0, daily_necessities: 1, groceries: 2, supplement: 3}
 
 end
