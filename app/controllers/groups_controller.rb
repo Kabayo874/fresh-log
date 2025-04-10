a001779df43959fb744e2e482b46553baa3289e1
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-04-10 13:13:29 +0000

    group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,36 @@
 class GroupsController < ApplicationController
-  def new
-  end
 
-  def index
+  def new
+    @group = Group.new
   end
 
   def show
+    @item = Item.new
+    @group = Group.find(params[:id])
   end
+
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render '/items'
+    end
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :discription, :group_image, :status)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
+  
 end
