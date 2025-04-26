186812d85737271c5c7ecba5e9aff9cc9297d64f
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-04-26 10:29:52 +0000

    [Finish]index

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,16 @@
 module ItemsHelper
+  def category_color_class(category)
+    case category
+    when "cosmetics"
+      "btn-cosmetics"
+    when "daily_necessities"
+      "btn-daily"
+    when "groceries"
+      "btn-food"
+    when "supplement"
+      "btn-supplement"
+    else
+      "btn-light"
+    end
+  end
 end
