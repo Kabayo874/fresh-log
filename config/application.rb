36c25cf0d03c580adf75ca99dd342fc934ec987d
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-09-01 01:37:50 +0000

    [Fix]Time-setting Tokyo

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module FreshLog
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # デフォルトのタイムゾーンを東京に設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
   end
 end
